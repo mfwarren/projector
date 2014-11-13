721b818eb0c85304b337852bebd60cb4c228dc06
Matt Warren <matt.warren@gmail.com>
2014-11-12 21:44:02 -0700

    remove old comments

--- projector/main/views.py
+++ projector/main/views.py
@@ -26,7 +26,6 @@ class ProjectListView(ListView):
 
 class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
-    # These next two lines tell the view to index lookups by username
     slug_field = "slug"
     slug_url_kwarg = "name"
 
@@ -46,24 +45,18 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
             assign_perm('owns_project', self.request.user, self.object)
         return HttpResponseRedirect(self.get_success_url())
 
-    # send the user back to their own page after a successful update
     def get_success_url(self):
         return reverse("main:project_detail",
                        kwargs={"name": self.object.slug})
 
 
 class ProjectUpdateView(LoginRequiredMixin, UpdateView):
-
     form_class = ProjectForm
-
-    # we already imported User in the view code above, remember?
     model = Project
 
-    # send the user back to their own page after a successful update
     def get_success_url(self):
         return reverse("main:project_detail",
                        kwargs={"name": self.object.slug})
 
     def get_object(self):
-        # Only get the User record for the user making the request
         return get_objects_for_user(self.request.user, 'main.owns_project').filter(slug=self.kwargs['slug']).first()
